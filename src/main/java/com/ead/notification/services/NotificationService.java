7a80713e3c337f3da31b87d427250955d5023756
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-04-09 15:45:33 -0300

    NotificationService Interface Initialized

--- src/main/java/com/ead/notification/services/NotificationService.java
+++ src/main/java/com/ead/notification/services/NotificationService.java
@@ -1,5 +1,8 @@
 package com.ead.notification.services;
 
+import com.ead.notification.models.NotificationModel;
+
 public interface NotificationService {
 
+    NotificationModel saveNotification(NotificationModel notificationModel);
 }
